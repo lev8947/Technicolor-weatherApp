6218747c34b6062829d893cbb1621c36b3ba54bf
Simon Levey <lev_47@hotmail.com>
2022-03-28 15:48:56 +0800

    weather function updates on console

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/hr";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+       this.fetchWeather(document.querySelector(".search-bar").value);
     }
+
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Perth");
